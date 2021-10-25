cae1517f3f3593fe8a3dfaca709ab0a36d673f5b
tayyabejaz200 <tayyabejaz700@gmail.com>
2021-10-25 15:27:45 +0500

    Added Node Functionality

--- src/node.go
+++ src/node.go
@@ -1,5 +1,37 @@
 package bitcoin_node
 
+import (
+	"fmt"
+	"net"
+)
+
 type Node struct {
-	SubVersion string `default:"Satoshi:0.7.2"`
+	Socket     net.Conn
+	peerAddr   net.IP
+	port       uint16
+	subVersion string
+}
+
+func CreateNode(peer_address string, port uint16) Node {
+	socket, _ := net.Dial("tcp4", fmt.Sprintf("%s:%d", peer_address, port))
+	return Node{
+		Socket:     socket,
+		peerAddr:   net.ParseIP(peer_address),
+		port:       port,
+		subVersion: "Satoshi:0.7.2",
+	}
+}
+
+func (n *Node) GetPeerAddress() string {
+	return n.peerAddr.String()
+}
+func (n *Node) GetPort() uint16 {
+	return n.port
+}
+func (n *Node) GetSubversion() string {
+	return n.subVersion
+}
+
+func (n *Node) Close() {
+	n.Socket.Close()
 }
